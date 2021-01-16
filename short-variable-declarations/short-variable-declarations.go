cb7eda6603703e89e76406c44a28519f0c00e043
nero-15 <ti7805@gmail.com>
2021-01-16 21:35:13 +0900

    add short variable declarations

---
+++ short-variable-declarations/short-variable-declarations.go
@@ -0,0 +1,12 @@
+// https://go-tour-jp.appspot.com/basics/10
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
