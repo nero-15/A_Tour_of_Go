0f1ec5438e6f065cdd06c097f76a34915ae50cf7
nero-15 <ti7805@gmail.com>
2021-01-17 11:39:35 +0900

    difer

---
+++ difer/difer.go
@@ -0,0 +1,14 @@
+// https://go-tour-jp.appspot.com/flowcontrol/13
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
