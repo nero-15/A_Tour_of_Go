852cf372525c86c4c527a99e7d17e42448d1ff9b
nero-15 <ti7805@gmail.com>
2021-01-19 21:52:45 +0900

    add slice

---
+++ nil-slices/nil-slices.go
@@ -0,0 +1,12 @@
+// https://go-tour-jp.appspot.com/moretypes/12
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
+}
