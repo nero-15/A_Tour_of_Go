3ef2bc445b015078f61240feabbf9bb1a687ba9c
nero-15 <ti7805@gmail.com>
2021-01-23 16:34:17 +0900

    add stringer

---
+++ exercise-stringer/exercise-stringer.go
@@ -0,0 +1,21 @@
+// https://go-tour-jp.appspot.com/methods/18
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr.
+func (a IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", a[0], a[1], a[2], a[3])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
