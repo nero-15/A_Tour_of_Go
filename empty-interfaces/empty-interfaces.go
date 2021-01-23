7d2ba841110a1b44fab97f341654a233b02f8a27
nero-15 <ti7805@gmail.com>
2021-01-23 16:13:38 +0900

    add interface

---
+++ empty-interfaces/empty-interfaces.go
@@ -0,0 +1,19 @@
+// https://go-tour-jp.appspot.com/methods/14
+package main
+
+import "fmt"
+
+func main() {
+	var i interface{}
+	describe(i)
+
+	i = 42
+	describe(i)
+
+	i = "hello"
+	describe(i)
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
