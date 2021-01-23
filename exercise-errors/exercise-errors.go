67aef9739a058c6750cae38795cdabb246a15c45
nero-15 <ti7805@gmail.com>
2021-01-23 16:48:04 +0900

    add error

---
+++ exercise-errors/exercise-errors.go
@@ -0,0 +1,29 @@
+// https://go-tour-jp.appspot.com/methods/20
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z0, z := 1.0, x
+	for math.Abs(z-z0)/z > 1e-15 {
+		z0, z = z, z-(z*z-x)/(2*z)
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
