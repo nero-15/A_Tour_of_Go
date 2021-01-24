75b5e2dcfa925ac60c01f22c02b19a1cef776bc7
nero-15 <ti7805@gmail.com>
2021-01-24 11:08:58 +0900

    add images

---
+++ images/images.go
@@ -0,0 +1,13 @@
+// https://go-tour-jp.appspot.com/methods/24
+package main
+
+import (
+	"fmt"
+	"image"
+)
+
+func main() {
+	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	fmt.Println(m.Bounds())
+	fmt.Println(m.At(0, 0).RGBA())
+}
