dcabe479357c9134ea104352527b5393767d08f1
nero-15 <ti7805@gmail.com>
2021-01-24 11:28:54 +0900

    add channels

---
+++ buffered-channes/buffered-channes.go
@@ -0,0 +1,12 @@
+// https://go-tour-jp.appspot.com/concurrency/3
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
---
+++ goroutines/goroutines.go
@@ -0,0 +1,19 @@
+// https://go-tour-jp.appspot.com/concurrency/1
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	go say("world")
+	say("hello")
+}
